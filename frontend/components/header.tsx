3a0569e9736eb5b856549375ed9012d6aacebeda
Daniel <jamoja64749@gmail.com>
2026-03-14 16:12:55 +0100

    chore(frontend): Remove unneded logic for a logo

--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -1,8 +1,4 @@
 'use client'
-import Link from 'next/link'
-import { Logo } from '@/components/logo'
-import { Menu, X } from 'lucide-react'
-import { Button } from '@/components/ui/button'
 import React from 'react'
 import { cn } from '@/lib/utils'
 
