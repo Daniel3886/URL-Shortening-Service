98c853333d960432070df0fe3032e39dc8fa19d8
Daniel <jamoja64749@gmail.com>
2026-03-15 21:26:35 +0100

    chore(frontend): remove unused dependencies in app page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,4 @@
-import { HeroHeader } from "@/components/header";
 import HeroSection from "@/components/hero-section";
-import Image from "next/image";
 
 export default function Home() {
   return (
