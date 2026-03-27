c73f1e35f7368588132a704468c2dea8b5b9a550
Daniel <jamoja64749@gmail.com>
2026-03-27 22:31:03 +0100

    fix(backend): move the shortCode and url fields to the correct places

--- backend/src/main/java/org/springboot/urlshorteningservice/dto/UrlResponse.java
+++ backend/src/main/java/org/springboot/urlshorteningservice/dto/UrlResponse.java
@@ -3,5 +3,5 @@ package org.springboot.urlshorteningservice.dto;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record UrlResponse(UUID id, String shortCode, String url,
+public record UrlResponse(UUID id, String url, String shortCode,
                    LocalDateTime createdAt, LocalDateTime updatedAt){}
--- backend/src/main/java/org/springboot/urlshorteningservice/dto/UrlStatsResponse.java
+++ backend/src/main/java/org/springboot/urlshorteningservice/dto/UrlStatsResponse.java
@@ -3,5 +3,5 @@ package org.springboot.urlshorteningservice.dto;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record UrlStatsResponse(UUID id, String shortCode, String url,
+public record UrlStatsResponse(UUID id, String url, String shortCode,
                           LocalDateTime createdAt, LocalDateTime updatedAt){}
