e15310a9a5c8d7310a1182532d80c878f0584fbc
Daniel <jamoja64749@gmail.com>
2026-03-29 19:04:00 +0200

    chore(backend): add exception handling for validation errors

--- backend/src/main/java/org/springboot/urlshorteningservice/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/org/springboot/urlshorteningservice/exception/GlobalExceptionHandler.java
@@ -2,10 +2,15 @@ package org.springboot.urlshorteningservice.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -22,6 +27,20 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors (
+            MethodArgumentNotValidException ex){
+        Map<String, String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .collect(Collectors.toMap(
+                        FieldError::getField,
+                        error -> Objects.requireNonNullElse(error.getDefaultMessage(), "invalid value")
+                ));
+
+        return ResponseEntity.badRequest().body(errors);
     }
 
     @ExceptionHandler(Exception.class)
